e879acb3aad9d600909ebbd2a249a55c359ea1ba
AnastasiaIllyashenko <119579234+AnastasiaIllyashenko@users.noreply.github.com>
2023-05-24 15:07:14 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int maxDigit = int.MinValue;
+        int minDigit = int.MaxValue;
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            maxDigit = Math.Max(maxDigit, digit);
+            minDigit = Math.Min(minDigit, digit);
+            number /= 10;
+        }
+
+        Console.WriteLine($"Максимальна цифра: {maxDigit}");
+        Console.WriteLine($"Мінімальна цифра: {minDigit}");
+    }
+}
